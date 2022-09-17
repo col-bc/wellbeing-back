876ce7a924419f7c43adfa41d80ce4db82f6fa93
colby <colby.b.cooper@gmail.com>
2022-09-17 00:38:39 -0400

    Update serve executable. Untrack sqlite db.

--- serve.sh
+++ serve.sh
@@ -1,7 +1,6 @@
 source ./venv/Scripts/activate
-echo ' ⚡ Activated virtual environment'
+echo ' * Activated virtual environment'
 export FLASK_DEBUG=1
-export FLASK_ENV=development
 export FLASK_APP=app.py
 
 flask run --port 5000
